54b2bed6e6be816d58c1b77a4466013202967876
Jim Gregory <jgreg@optonline.net>
2025-04-26 10:26:51 -0400

    File Cleanup/Organization

--- src/xx_examples/imgui/autopilot_ui.h
+++ src/xx_examples/imgui/autopilot_ui.h
@@ -3,22 +3,19 @@
 // •                                    COPYRIGHT (c) 2025  -  ALL RIGHTS RESERVED
 // •••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 
-
-#ifndef TESTS_ALL_H
-#define TESTS_ALL_H
-
-
-#ifdef ENABLE_TESTS
-
-// Master test entry point
-void runAllTests();
-
-// Command registration helpers
-void registerTestCommand();
-void unregisterTestCommand();
-
-#endif
+#ifndef AUTOPILOT_UI_H
+#define AUTOPILOT_UI_H
 
 
+#include "XPLMDisplay.h"
 
-#endif //TESTS_ALL_H
+
+void InitAutopilotUI();      // Called once in `XPluginStart()` for any setup logic
+int DrawAutopilotUIWindow(XPLMDrawingPhase inPhase, int inIsBefore, void* inRefcon);  // Draws the autopilot UI window
+
+// Callback Registration/Unregistration Functions
+void RegisterAutopilotUI();   // Registers the drawing callback
+void UnregisterAutopilotUI(); // Unregisters the drawing callback
+
+
+#endif //AUTOPILOT_UI_H
